b4ac743bc173f74e0cca8d8e23953e36e64791ae
Paul Grant <poglesbyg@gmail.com>
2014-11-06 16:37:57 +0100

    work need doing

--- INFO132-Oblig2/oblig2.pl
+++ INFO132-Oblig2/oblig2.pl
@@ -12,27 +12,81 @@ lesFil_hp(Stream,[X|L]) :-
     read(Stream,X),
     lesFil_hp(Stream,L).
 
-splitLines([end_of_file], []).
-splitLines([Name, Pop, Status, Fact, CoordX, CoordY | Rest], [city(Name, Pop, Status, Fact, CoordX, CoordY) | Return]) :-
-  splitLines(Rest,Return).
+readStead :-
+  lesFil('stedsinfo.txt',Read),
+  createPlaces(Read).
+
+createPlaces([end_of_file]).
+createPlaces([Name, Pop, Status, Fact, CoordX, CoordY | Rest]) :-
+  assert(city(Name, Pop, Status, Fact, CoordX, CoordY)),
+  createPlaces(Rest).
 
 % beskriv/1 takes in a city name and prints out a short 
 % description of it
-beskriv([]).
-beskriv(Place) :-
-  Place = city(Name, Pop, Status, Fact, CoordX, CoordY),
+beskriv(Name) :-
+  city(Name, Pop, Status, Fact, CoordX, CoordY),
   write(Name), write(' er en '), write(Status),
   write(' med '), write(Pop), write(' innbyggere.'),nl,
-  write(Fact),nl,nl.
+  write(Fact).
 
-listByer([end_of_file],[]).
-listByer([Name, Pop, Status, Fact, CoordX, CoordY | Rest], [city(Name, Pop, Status, Fact, CoordX, CoordY) | Return]) :-
-  write(Name), nl,
-  listByer(Rest, Return).
+% listByer/1 gives a list of all the cities
+
+listByer(List) :-
+  findall(Name, city(Name, _,_,_,_,_),List).
+
+
+readRoutes :-
+  lesFil('reiseruter-2.txt',Read),
+  createRoutes(Read).
+
+createRoutes([end_of_file]).
+createRoutes([Start, End, Transportation, TimeInMin, Price | Rest]) :-
+  assert(route(Start, End, Transportation, TimeInMin, Price)),
+  createRoutes(Rest).
+
+% rute/2 makes sure a route is found between cities
+rute(Start,End) :-
+  route(Start,End,_,_,_).
+
+% astar algorithm
+
+bestfirst(Start,Sol) :-
+     expand([path([Start],0,0)],Sol).
+
+expand([path([N|Tail],_,_)|_],[N|Tail]) :- goal(N),!.
+expand([path([N|Tail],G,_)|Rest],Sol) :-
+     findall(M/C,(s(N,M,C),\+member(M,Tail)),Succ),
+     succlist(G,Succ,[N|Tail],Paths),
+     addsucclist(Paths,Rest,Newpaths),
+     expand(Newpaths,Sol).
+     
+succlist(_,[],_,[]).
+succlist(G0,[N/C|Rest],Tail,[path([N|Tail],G,F)|Restpaths]) :-
+     G is G0+C,
+     h(N,H),
+     F is G+H,
+     succlist(G0,Rest,Tail,Restpaths).
+
+addsucclist([],Newpaths,Newpaths).
+addsucclist([Head|Tail],Rest,Newpaths) :-
+     insertordered(Head,Rest,Paths),
+     addsucclist(Tail,Paths,Newpaths).
+     
+insertordered(P0,[P1|Rest],[P0,P1|Rest]) :-
+     f(P0,F0),
+     f(P1,F1),
+     F0 < F1,!.
+insertordered(P0,[P1|Rest], [P1|Newrest]) :-
+     insertordered(P0,Rest,Newrest).
+insertordered(P,[],[P]).
+
+f(path(_,_,F),F).
+g(path(_,G,_),G).
+     
+
+%finnReiseRaskest/2
+finnReiseRaskest(Start, End) :-
+  bestfirst(f(path(_,_,Start),Start),g(path(_,End,_),End)).
 
 main :-
-  lesFil('stedsinfo.txt', Lines),
-  splitLines(Lines, Return),
-  maplist(beskriv,Return),
-  listByer(Lines,Return).
-
+  write('Hello World!').
